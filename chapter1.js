2940a7b4196976660e5f4e25f7fe42856e63e58a
youngbamer <0421mk@naver.com>
2021-10-15 00:32:09 +0900

    HTML 렌더링 메서드 생성

--- chapter1.js
+++ chapter1.js
@@ -27,6 +27,26 @@ var invoices = {
 // 공연 요청이 들어오면 연극의 장르와 관객 규모를 기초로 비용 책정
 // 공연료와 별개로 포인트를 지급해서 다음번 의뢰 시 공연료 할인 가능
 
+function htmlStatement(invoice, plays) {
+	return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+	let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+	result += "<table>\n";
+	result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+	for (let perf of data.performances) {
+		result += `<tr><td>${perf.play.name}</td>
+		<td>${perf.play.audience}석</td>
+		<td>${usd(perf.amount)}</td></tr>\n`;
+	}
+	result += "</table>\n";
+	result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+	result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+
+	return result;
+}
+
 // 공연료 청구서를 출력하는 코드
 function statement(invoice, plays) {
 
@@ -123,15 +143,16 @@ function renderPlainText(data) {
 	result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
 	return result;
 
-	// format => usd 메서드로 따로 생성하고 단위 변환 로직(/100)도 이동
-	function usd(aNumber) {
-		return new Intl.NumberFormat("en-US", {
-			style: "currency", currency: "USD",
-			minimumFractionDigits: 2
-		}).format(aNumber / 100);
-	}
+}
 
+// format => usd 메서드로 따로 생성하고 단위 변환 로직(/100)도 이동
+function usd(aNumber) {
+	return new Intl.NumberFormat("en-US", {
+		style: "currency", currency: "USD",
+		minimumFractionDigits: 2
+	}).format(aNumber / 100);
 }
 
 // 테스트 코드
 console.log(statement(invoices, plays));
+console.log(htmlStatement(invoices, plays));
